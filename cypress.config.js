89e5828ab56f1601fc757535be5ecb9d6653c9a7
Maitry.Shah <maitry.shah.ext@seamless.se>
2023-05-24 17:34:30 +0530

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl:'https://web.vepaar.com/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here 
-      //  require('d:/IntegrateJenkinsAndGit/cypress/plugins')(on);
     },
     video:false,
   },
